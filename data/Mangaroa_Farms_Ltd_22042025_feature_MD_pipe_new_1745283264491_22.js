0a05b784e76487c81f43c13a09779a73ff674525
technosophical <132953111+technosophical@users.noreply.github.com>
2025-04-28 22:06:06 +1200

    bulk

---
+++ data/Mangaroa_Farms_Ltd_22042025_feature_MD_pipe_new_1745283264491_22.js
@@ -0,0 +1,8 @@
+var json_Mangaroa_Farms_Ltd_22042025_feature_MD_pipe_new_1745283264491_22 = {
+"type": "FeatureCollection",
+"name": "Mangaroa_Farms_Ltd_22042025_feature_MD_pipe_new_1745283264491_22",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "FarmName": "Mangaroa Farms Ltd", "FeatType": "MD pipe new", "FeatName": "MD pipe new Dan" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 175.08737, -41.14661 ], [ 175.08855, -41.14616 ], [ 175.0897, -41.1448 ], [ 175.09024, -41.14446 ] ] ] } }
+]
+}
---
+++ data/Mangaroa_Farms_Ltd_22042025_feature_old_water_line_1745283110117_21.js
@@ -0,0 +1,8 @@
+var json_Mangaroa_Farms_Ltd_22042025_feature_old_water_line_1745283110117_21 = {
+"type": "FeatureCollection",
+"name": "Mangaroa_Farms_Ltd_22042025_feature_old_water_line_1745283110117_21",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "FarmName": "Mangaroa Farms Ltd", "FeatType": "old water line", "FeatName": "old water line" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 175.09086, -41.14798 ], [ 175.09071, -41.14666 ], [ 175.0906, -41.14641 ] ] ] } }
+]
+}
