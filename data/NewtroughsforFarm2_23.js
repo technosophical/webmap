48124d7504363208e0ebf7801de5402350645710
technosophical <132953111+technosophical@users.noreply.github.com>
2025-04-28 17:40:21 +1200

    bulk upload

---
+++ data/NewtroughsforFarm2_23.js
@@ -0,0 +1,21 @@
+var json_NewtroughsforFarm2_23 = {
+"type": "FeatureCollection",
+"name": "NewtroughsforFarm2_23",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "FarmName": "Mangaroa Farms Ltd", "FeatType": "new troughs for Phils", "FeatName": "New 1" }, "geometry": { "type": "Point", "coordinates": [ 175.09249, -41.14072 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "FarmName": "Mangaroa Farms Ltd", "FeatType": "new troughs for Phils", "FeatName": "new troughs for Phils 2" }, "geometry": { "type": "Point", "coordinates": [ 175.09348, -41.14143 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "FarmName": "Mangaroa Farms Ltd", "FeatType": "new troughs for Phils", "FeatName": "new troughs for Phils 3" }, "geometry": { "type": "Point", "coordinates": [ 175.0933, -41.14018 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "FarmName": "Mangaroa Farms Ltd", "FeatType": "new troughs for Phils", "FeatName": "new troughs for Phils 4" }, "geometry": { "type": "Point", "coordinates": [ 175.0928, -41.13949 ] } },
+{ "type": "Feature", "properties": { "fid": "5", "FarmName": "Mangaroa Farms Ltd", "FeatType": "new troughs for Phils", "FeatName": "new troughs for Phils 5" }, "geometry": { "type": "Point", "coordinates": [ 175.09471, -41.1372 ] } },
+{ "type": "Feature", "properties": { "fid": "6", "FarmName": "Mangaroa Farms Ltd", "FeatType": "new troughs for Phils", "FeatName": "new troughs for Phils 6" }, "geometry": { "type": "Point", "coordinates": [ 175.09531, -41.13905 ] } },
+{ "type": "Feature", "properties": { "fid": "7", "FarmName": "Mangaroa Farms Ltd", "FeatType": "new troughs for Phils", "FeatName": "new troughs for Phils 7" }, "geometry": { "type": "Point", "coordinates": [ 175.09241, -41.13884 ] } },
+{ "type": "Feature", "properties": { "fid": "8", "FarmName": "Mangaroa Farms Ltd", "FeatType": "new troughs for Phils", "FeatName": "new troughs for Phils 8" }, "geometry": { "type": "Point", "coordinates": [ 175.09497, -41.1402 ] } },
+{ "type": "Feature", "properties": { "fid": "9", "FarmName": "Mangaroa Farms Ltd", "FeatType": "new troughs for Phils", "FeatName": "new troughs for Phils 9" }, "geometry": { "type": "Point", "coordinates": [ 175.09583, -41.14119 ] } },
+{ "type": "Feature", "properties": { "fid": "10", "FarmName": "Mangaroa Farms Ltd", "FeatType": "new troughs for Phils", "FeatName": "new troughs for Phils 10" }, "geometry": { "type": "Point", "coordinates": [ 175.09436, -41.14238 ] } },
+{ "type": "Feature", "properties": { "fid": "11", "FarmName": "Mangaroa Farms Ltd", "FeatType": "new troughs for Phils", "FeatName": "new troughs for Phils 11" }, "geometry": { "type": "Point", "coordinates": [ 175.09464, -41.14361 ] } },
+{ "type": "Feature", "properties": { "fid": "12", "FarmName": "Mangaroa Farms Ltd", "FeatType": "new troughs for Phils", "FeatName": "new troughs for Phils 12" }, "geometry": { "type": "Point", "coordinates": [ 175.09676, -41.14383 ] } },
+{ "type": "Feature", "properties": { "fid": "13", "FarmName": "Mangaroa Farms Ltd", "FeatType": "new troughs for Phils", "FeatName": "new troughs for Phils 13" }, "geometry": { "type": "Point", "coordinates": [ 175.09748, -41.1443 ] } },
+{ "type": "Feature", "properties": { "fid": "14", "FarmName": "Mangaroa Farms Ltd", "FeatType": "new troughs for Phils", "FeatName": "new troughs for Phils 14" }, "geometry": { "type": "Point", "coordinates": [ 175.09849, -41.14476 ] } }
+]
+}
